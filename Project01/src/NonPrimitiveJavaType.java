5d8e11bc03a504b480061dc79e621ff87081a782
Vaibhav Kolte <vaibhavkolte95@gmail.com>
2025-04-22 20:27:05 +0530

    start course

---
+++ Project01/src/NonPrimitiveJavaType.java
@@ -0,0 +1,16 @@
+
+public class NonPrimitiveJavaType {
+
+	public static void main(String[] args) {
+		int aNumber = 9;
+		double pi = 3.14;
+		String greet = "Good";
+		String blank = " ";
+		String greetType = "Morning";
+		
+		System.out.println(greet + blank + greetType);
+		
+		System.out.println("Value: " + aNumber + pi);
+	}
+
+}	
---
+++ Project01/src/Variables.java
@@ -0,0 +1,27 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int aValue;
+		
+		aValue = 10;
+		
+		short aShort = 93;		
+		long aLong = 8205;
+		double aDouble = 3.14;		
+		float aFloat = 3.14f;
+		char aChar = 'v';
+		boolean aBoolean = true;
+		byte aByte = 127;
+		
+		System.out.println(aValue);
+		System.out.println(aShort);
+		System.out.println(aLong);
+		System.out.println(aDouble);
+		System.out.println(aFloat);
+		System.out.println(aChar);
+		System.out.println(aBoolean);
+		System.out.println(aByte);
+	}
+
+}
